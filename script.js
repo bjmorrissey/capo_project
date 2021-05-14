2e945d6129e3bc2a1359e438f2f75b889c9d97d1
Brian <brianjmorrissey@gmail.com>
2021-05-14 09:46:26 -0500

    added guitar neck graphic

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-let tuningtype = document.getElementById('tuningtype');
-let notes = document.querySelector('.notes');
-
-
-let capoField = document.getElementById('capoPlacement');
+const tuningtype = document.getElementById('tuningtype');
+const notes = document.querySelector('.notes');
+const capoField = document.getElementById('capoPlacement');
 let capoChoice = 0;
-let transposeBtn = document.querySelector('.transpose');
+const transposeBtn = document.querySelector('.transpose');
+const hidden = document.querySelector('.hidden ');
+const openNotes = document.querySelector('.opennotes');
+const firstFret = document.querySelector('.fret1');
+const secondFret = document.querySelector('.fret2');
+const thirdFret = document.querySelector('.fret3');
+const fourthFret = document.querySelector('.fret4');
+const fifthFret = document.querySelector('.fret5');
+let nutNotes = [];
+let first = [];
+let second = [];
+let third = [];
+let fourth = [];
+let fifth = [];
 
 //Notes array
 const notesSharp = [
@@ -84,16 +95,89 @@ function noteLoop(noteIndex) {
 }
 
 //Calcs the note changes if a capo were applied to a guitar neck in a certain tuning
+// function capoCalc(capoPlacement, tuning) {
+//   let capoedNotes = [];
+//   let tuningName = tuning.name;
+//   let firstFret = [];
+
+
+//   tuning.notes.forEach((note) => {
+//     let noteIndex = notesSharp.indexOf(note) + capoPlacement;
+//     let firstIndex = notesSharp.indexOf(note + 1)
+//     noteIndex = noteLoop(noteIndex);
+//     firstIndex = noteLoop(firstIndex)
+//     capoedNotes.push(notesSharp[noteIndex]);
+//     firstFret.push(notesSharp[firstIndex])
+//   });
+
+
+
+//   let suffix = 'th';
+//   if (capoPlacement === 1) {
+//     suffix = 'st'
+//   } else if (capoPlacement === 2) {
+//     suffix = 'nd';
+//   } else if (capoPlacement === 3) {
+//     suffix = 'rd';
+//   }
+
+//   if (capoPlacement > 12) {
+//     notes.innerHTML = "Please choose a lower fret. Going above this is crazy and you'll likely have intonation issues brah... 🎸"
+//   } else {
+//     let statement = '';
+//     if (capoPlacement === 0) {
+//       statement = `If you had an open guitar neck, `
+//     } else {
+//       statement = `If you put your capo on the ${capoPlacement}${suffix} fret, `
+//     }
+
+//   };
+//   //Return an array of notes
+//   nutNotes = capoedNotes;
+//   return capoedNotes;
+//   return firstFret;
+
+// };
+
 function capoCalc(capoPlacement, tuning) {
   let capoedNotes = [];
-  let tuningName = tuning.name;
+  let firstFret = [];
+  let secondFret = [];
+  let thirdFret = [];
+  let fourthFret = [];
+  let fifthFret = [];
 
 
   tuning.notes.forEach((note) => {
     let noteIndex = notesSharp.indexOf(note) + capoPlacement;
+    let firstIndex = noteIndex + 1;
+    let secondIndex = noteIndex + 2;
+    let thirdIndex = noteIndex + 3;
+    let fourthIndex = noteIndex + 4;
+    let fifthIndex = noteIndex + 5;
     noteIndex = noteLoop(noteIndex);
+    firstIndex = noteLoop(firstIndex);
+    secondIndex = noteLoop(secondIndex);
+    thirdIndex = noteLoop(thirdIndex);
+    fourthIndex = noteLoop(fourthIndex);
+    fifthIndex = noteLoop(fifthIndex);
     capoedNotes.push(notesSharp[noteIndex]);
+    firstFret.push(notesSharp[firstIndex])
+    secondFret.push(notesSharp[secondIndex])
+    thirdFret.push(notesSharp[thirdIndex])
+    fourthFret.push(notesSharp[fourthIndex])
+    fifthFret.push(notesSharp[fifthIndex])
   });
+
+  //Return an array of notes
+  nutNotes = capoedNotes;
+  first = firstFret;
+  second = secondFret;
+  third = thirdFret;
+  fourth = fourthFret;
+  fifth = fifthFret;
+
+  // return `${capoedNotes}, ${firstFret}, ${secondFret}, ${thirdFret}, ${fourthFret}, ${fifthFret}`;
 
   let suffix = 'th';
   if (capoPlacement === 1) {
@@ -109,18 +193,29 @@ function capoCalc(capoPlacement, tuning) {
   } else {
     let statement = '';
     if (capoPlacement === 0) {
-      statement = `If you had an open guitar neck, `
+      statement = `If you had an open guitar neck: `
     } else {
-      statement = `If you put your capo on the ${capoPlacement}${suffix} fret, `
+      statement = `If you put a capo on the ${capoPlacement}${suffix} fret:  `
     }
-
-    notes.innerHTML = statement + `the notes from <em>lowest to highest</em> are <br><u>${capoedNotes}</u> in ${tuningName}.`;
-
-
-  }
-  return capoedNotes;
+    notes.innerHTML = statement;
+  };
 }
 
+
+
+function noteLayout(notearr) {
+  let fretnotes = '';
+  notearr.forEach(note => {
+    fretnotes += `<li>${note}</li>`;
+  });
+  return fretnotes;
+}
+
+function fretRep() {
+  let fret = '<ul>';
+  fret += `<li>Hello there</li></ul>`
+
+}
 
 //Event listener for tuning type
 
@@ -156,46 +251,34 @@ tuningtype.addEventListener('click', (e) => {
 });
 
 transposeBtn.addEventListener('click', (e) => {
-
-  console.log(capoCalc(capoChoice, tuneChoice))
-
+  capoCalc(capoChoice, tuneChoice);
+  hidden.classList.remove('hidden');
+  openNotes.innerHTML = `<ul>
+    ${noteLayout(nutNotes)}
+  </ul>`
+  firstFret.innerHTML = `<ul>
+    ${noteLayout(first)}
+  </ul>`
+  secondFret.innerHTML = `<ul>
+    ${noteLayout(second)}
+  </ul>`
+  thirdFret.innerHTML = `<ul>
+    ${noteLayout(third)}
+  </ul>`
+  fourthFret.innerHTML = `<ul>
+    ${noteLayout(fourth)}
+  </ul>`
+  fifthFret.innerHTML = `<ul>
+    ${noteLayout(fifth)}
+  </ul>`
+  document.getElementById('guitarneck').style.visibility = 'visible'
 });
 
 
-//Maps string notes out to find base notes
-// function stringMap(note) {
-//   let stringNotes = [];
 
-//   for (i = 0; i < 6; i++) {
-//     let strNotes = [];
-//     let root = note.notes[i];
-//     let rootIndex = notesSharp.indexOf(root);
-//     let start = noteLoop(rootIndex);
-//     for (j = 0; j <= 12; j++) {
-//       let counter = noteLoop(start + j);
-//       strNotes.push(notesSharp[counter]);
-//       strNotes.join(' ');
-//     }
-//     stringNotes.push(strNotes);
-//     stringNotes.push('<br>');
-//   }
-//   return stringNotes;
-// }
-
-// /notes.innerHTML = stringMap(tunings.standard);
-//console.log(stringMap(tunings.dadgad));
-
-//Maps out all six strings of guitar.
-// let noteMap = (tuning) =>
-//   tuning.notes.forEach((note) => {
-//     console.log(stringMap(note));
-//   });
-
-// function guitarNeckMap(tuning) {
-//   let str = '';
-//   for (let i = 0; i < 20; i++) {
-//     str += noteMap(tuning);
-//   }
-//   return str;
-// }
-// 
+// console.log(`Notes at nut: ${nutNotes}`)
+// console.log(`Notes at first: ${first}`)
+// console.log(`Notes at second: ${second}`)
+// console.log(`Notes at third: ${third}`)
+// console.log(`Notes at fourth: ${fourth}`)
+// console.log(`Notes at fifth: ${fifth}`)
